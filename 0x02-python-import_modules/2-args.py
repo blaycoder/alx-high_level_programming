cfd1552c628addc5db763e0a8e349d7796dbf278
blaycoder <onatola4real@gmail.com>
2022-06-08 18:31:58 -0700

    new changes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,13 @@
+#!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 1:
+
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
-    elif len(sys.argv) == 2:
-        print("{} argument:".format(1))
-        print("{}: {}".format(1, sys.argv[1]))
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
-        for i in range(1, len(sys.argv)):
-            print("{}: {}".format(i, sys.argv[i]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
