bcbeefc2ed00976dcb0097f25b57d3ade2677994
blaycoder <onatola4real@gmail.com>
2022-06-22 19:13:37 -0700

    added new tasks

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Define a class Square"""
+
+
+class Square:
+    """Initialize the Square class"""
+    def __init__(self):
+        """execute an empty class"""
+        pass
