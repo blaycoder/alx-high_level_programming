ed4934724893ecef6c01a679520cab34fd4a5db7
blaycoder <onatola4real@gmail.com>
2022-09-05 18:30:28 +0000

    new changes added

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 let first_line = "C is fun";
 let second_line = "Python is cool";
-let third_line = "Javascript is amazing"
+let third_line = "JavaScript is amazing"
 console.log(`${first_line}\n${second_line}\n${third_line}`);
