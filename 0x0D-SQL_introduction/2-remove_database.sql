c1c260c5e1c443a1ac57940b37bf5305442d639a
blaycoder <onatola4real@gmail.com>
2022-09-12 15:39:20 -0700

    new changes added

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
--- deletes a database in MySQL server, if database exists, script should not fail
-DROP DATABASE IF NOT EXISTS hbtn_0c_0
+-- deletes a database in MySQL serveR
+DROP DATABASE IF NOT EXISTS hbtn_0c_0;
