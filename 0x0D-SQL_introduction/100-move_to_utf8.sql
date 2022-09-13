2c9df9b97c0222d22651b3ff8bd4129b4778840a
blaycoder <onatola4real@gmail.com>
2022-09-12 22:56:23 -0700

    made new changes

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,5 +2,8 @@
 USE `htbn_0c_0`
 ALTER TABLE `first_table`
 CONVERT TO CHARACTER SET utfmb8 COLLATE  utfmb8_general_ci;
-ALTER name in TABLE `first_table`
-CONVERT TO CHARACTER SET utfmb8 COLLATE utfmb8_general_ci;
+ALTER DATABASE htbn_0c_0
+CHARACTER SET = utfmb8
+COLLATE = utfmb8_general_ci;
+
+
