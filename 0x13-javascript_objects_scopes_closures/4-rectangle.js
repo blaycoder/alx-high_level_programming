f79daa695c00576aa17ed28c4e13eb7f2c631b1c
blaycoder <onatola4real@gmail.com>
2022-09-17 08:22:59 -0700

    new file added

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+module.exports = class Rectangle{
+	constructor(w, h){
+	    if(w > 0 && h > 0){
+		this.width = w;
+		this.height = h;
+	    }
+	}
+	print(){
+	    let str = 'X';
+	    let multistr = str.repeat(this.width);
+	    for(let i = 0; i <this.height; i++){
+		console.log(multistr);
+	    }
+    }
+    rotate(){
+	let temp = this.width;
+	this.width = this.height;
+	temp = this.height;
+    }
+    double(){
+	this.width = this.width * 2;
+	this.height = this.height * 2;
+    }
+}
