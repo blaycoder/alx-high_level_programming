6c8c76bd6192dfbb265906ebd7206e62683d2eb1
blaycoder <onatola4real@gmail.com>
2022-09-17 08:40:06 -0700

    new file added

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const Rectangle = require('./4-rectangle');
-modules.exports = class Square extends Rectangle{
+module.exports = class Square extends Rectangle{
     constructor(size){
-	this.size = size;
-	super()
+	super(size,size);
     }
+
 }
