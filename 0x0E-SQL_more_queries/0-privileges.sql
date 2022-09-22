fa5430964675af95fdb4bdd6d9b2f4ef373e9e44
blaycoder <onatola4real@gmail.com>
2022-09-21 20:39:37 -0700

    made changes

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,8 @@
 -- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
 -- show privileges for user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS
+     FOR 'user_0d_1'@'localhost';
 --show privileges for user_0d_2
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS
+     FOR 'user_0d_2'@'localhost';
 
