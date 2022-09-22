f02ca3f1de8c5da7eca7982616ea76668d8e9b83
blaycoder <onatola4real@gmail.com>
2022-09-22 02:28:33 -0700

    made changes

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -5,5 +5,5 @@ SELECT tv_shows.title, tv_genres.name
 	    ON tv_shows.id = tv_show_genres.show_id
 
 	    LEFT JOIN tv_genres
-	    ON tv_shows.id = tv_genres.id
+	    ON tv_shows.genre_id = tv_genres.id
 	 ORDER BY tv_shows.title, tv_genre.name ASC;
